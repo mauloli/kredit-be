a6749e21d38ce4eb711df3a24f7a4fceb45f5a4a
maul <maulanasholihin@Maulanas-MacBook-Pro.local>
2025-08-10 15:30:18 +0700

    disable pagination

--- src/services/vehicle/vehicle.hooks.js
+++ src/services/vehicle/vehicle.hooks.js
@@ -1,11 +1,14 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { disablePagination } = require('feathers-hooks-common');
 
 module.exports = {
   before: {
     all: [
       authenticate('jwt'),
     ],
-    find: [],
+    find: [
+      disablePagination()
+    ],
     get: [],
     create: [],
     update: [],
